80af2cd8d1bc1fc0eae5f35a037d4d4ae6b05bdd
Summer <511391805@qq.com>
2022-03-01 13:20:12 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Layout from "./pages/Layout.vue";
+</script>
+
+<template>
+  <layout/>
+</template>
+
+<style>
+
+</style>
---
+++ src/pages/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-toolbar class="bg-primary text-white">
+    <q-btn flat round dense icon="menu" />
+    <q-toolbar-title>
+      音乐盒
+    </q-toolbar-title>
+    <q-btn flat round dense icon="more_vert" />
+  </q-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Layout"
+}
+</script>
+
+<style scoped>
+
+</style>
